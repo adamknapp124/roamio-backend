00d6d09605c6425025a27f439fb738de6044dfe6
Adam Knapp <aknapp124@gmail.com>
2024-03-30 00:08:38 -0700

    Added cloudinary

--- index.js
+++ index.js
@@ -1,12 +1,73 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 4000;
 const cors = require('cors');
 
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+const PORT = process.env.PORT;
 
-app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
+const cloudinary = require('./cloudinary/cloudinary');
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	database: 'road_trip',
+	password: 'Talon511!',
+});
+
+app.use(cors());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+
+app.post('/', async (req, res) => {
+	// destructure the properties from the req object
+	const { image } = req.body;
+	try {
+		const uploadedImage = await cloudinary.uploader.upload(
+			image,
+			{
+				upload_preset: 'unsigned_upload',
+				allowed_formats: ['png', 'jpg', 'jpeg', 'svg', 'ico', 'jfif', 'webp'],
+			},
+			function (error, result) {
+				if (error) {
+					console.log('Error: ', error);
+					res.status(500).json({ error: 'Failed to upload image' });
+					return;
+				}
+
+				console.log(result);
+				res.status(200).json(result);
+			}
+		);
+	} catch (error) {
+		return;
+	}
+});
+
+app.post('/add-photo', async (req, res) => {
+	const data = req.body;
+	const public_id = data.public_id;
+	try {
+		const query = 'INSERT INTO photos (public_id) VALUES (?)';
+		const values = [public_id];
+		connection.query(query, [values]);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get('/get-public-ids', async (req, res) => {
+	const query = 'SELECT * FROM photos';
+	connection.query(query, function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		res.send(result);
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
 });
